3de043898c8650eb07f9e2131ddf321204c2ae88
Andrea Passini <andreapa98@gmail.com>
2024-03-06 22:04:31 +0100

    project setup
    Setup project with Premake
    
    Update .gitignore
    
    Update .gitignore
    
    Update .gitignore
    
    imgui
    
    readme
    
    Premake and glad
    
    Importing assimp
    
    Importing assimp
    
    Importing assimp
    
    Importing assimp
    
    Importing assimp
    
    Create anim.h
    
    Create ai_assert.h
    
    Create aabb.h
    
    VS project and GLFW, GLAD import

---
+++ README.md
@@ -0,0 +1,18 @@
+DROP
+
+a simple game engine
+
+
+# Setup
+
+This set-up works for **Windows x64**
+
+### 1. Download static libraries
+
+Download the directory "dependencies"
+
+https://drive.google.com/drive/folders/1BnptvQUHHxcnXt53LJGVPdqLg2EXvkPA?usp=sharing
+
+### 2. Generate the vs solutions and projects
+
+- double click on "GenerateProjects.bat"
---
+++ premake5.lua
@@ -0,0 +1,96 @@
+workspace "DROP"
+    architecture "x64"
+
+    configurations{
+        "Debug",
+        "Release"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}" -- BuildName/x64/Windows
+
+
+--- include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["glad"] = "dependencies/glad/include"
+IncludeDir["GLFW"] = "dependencies/GLFW/include"
+
+include "dependencies/glad"
+include "dependencies/GLFW"
+
+project "DROP"
+    location "DROP"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files{
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs{
+        -- "dependencies/glfw/include",
+        "dependencies/glm/include",
+        "dependencies/stb_img/include",
+        "dependencies/assimp/includes",
+
+        "%{IncludeDir.glad}",
+        "%{IncludeDir.GLFW}"
+    }
+
+    libdirs { 
+        -- "dependencies/glfw/lib-vc2022",
+        "dependencies/assimp/libs"
+    }
+
+
+    links{  --- The name of the projects
+        -- glad
+        "glad",
+    
+        "GLFW",
+        -- "glfw3.lib",
+        -- "glfw3_mt.lib",
+        -- "opengl32.lib",
+        "Opengl32.lib",
+
+        -- ASSIMP
+        "assimp-vc143-mt.lib",
+        "draco.lib",
+        "kubazip.lib",
+        "minizip.lib",
+        "poly2tri.lib",
+        "pugixml.lib",
+        "zlib.lib",
+
+        "gdi32.lib",
+        "user32.lib",
+        "Shell32.lib",
+        "Advapi32.lib"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0"
+
+        defines{
+            "DROP_PLATFORM_WINDOWS",
+            "_UNICODE",
+            "UNICODE"
+        }
+
+    filter "configurations:Debug"
+        defines "DROP_DEBUG"
+        symbols "On"
+        buildoptions "/MT"
+
+    filter "configurations:Release"
+        defines "DROP_RELEASE"
+        optimize "On"
+        buildoptions "/MT"
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
