d6331f2f98e165ce10184f016a30e9c5b3b2e9e4
Andrea Passini <andreapa98@gmail.com>
2024-12-30 17:45:04 +0100

    Update scalar.h

--- DROP/src/DROP/math/scalar.h
+++ DROP/src/DROP/math/scalar.h
@@ -13,6 +13,10 @@ inline Scalar mix(Scalar a, Scalar b, Scalar t){
     return a*(1-t) + b*t;
 }
 
+inline Scalar lerp(Scalar a, Scalar b, Scalar t) {
+    return a * (1 - t) + b * t;
+}
+
 inline bool areEqual( Scalar a, Scalar b ){
     return std::abs(a-b)<EPSILON;
 }
@@ -21,6 +25,11 @@ inline bool areEqual( Scalar a, Scalar b ){
 inline Scalar randomBetween( Scalar min, Scalar max ){
     Scalar rand0to1 = (rand() % 1001) / 1000.0;
     return mix( min , max, rand0to1 );
+}
+
+inline Scalar randomInRange(const Scalar min, const Scalar max) {
+    Scalar random0to1 = (Scalar)std::rand() / RAND_MAX;
+    return lerp(min, max, random0to1);
 }
 
 };
