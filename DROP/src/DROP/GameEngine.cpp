e72b47c80f1596a3a062186910f7c8aaf57383d7
Andrea Passini <andreapa98@gmail.com>
2025-11-05 21:55:09 +0100

    Update GameEngine.cpp

--- DROP/src/DROP/GameEngine.cpp
+++ DROP/src/DROP/GameEngine.cpp
@@ -14,6 +14,7 @@ bool g_GameEngineRunning = true;
 
 namespace Drop {
 
+// MOVE THE WINDOWS STUFF TO PLATFORM LAYER
 // https://learn.microsoft.com/it-it/windows/win32/dlls/using-run-time-dynamic-linking
 void LinkGameLibrary(HINSTANCE& inHinstLib, GameDLLProAdresses& InGameFunctions)
 {
@@ -75,8 +76,75 @@ void FreeGameLibrary(HINSTANCE& inHinstLib, GameDLLProAdresses& InGameFunctions)
     InGameFunctions.UpdateGame = UpdateGameStub;
 }
 
+static void CatStrings(size_t SourceACount, char* SourceA,
+    size_t SourceBCount, char* SourceB,
+    size_t DestCount, char* Dest)
+{
+    // TODO(casey): Dest bounds checking!
+
+    for (int Index = 0;
+        Index < SourceACount;
+        ++Index)
+    {
+        *Dest++ = *SourceA++;
+    }
+
+    for (int Index = 0;
+        Index < SourceBCount;
+        ++Index)
+    {
+        *Dest++ = *SourceB++;
+    }
+
+    *Dest++ = 0;
+}
+
+
+inline FILETIME Win32GetLastWriteTime(char* Filename)
+{
+    FILETIME LastWriteTime = {};
+
+    WIN32_FIND_DATA FindData;
+    HANDLE FindHandle = FindFirstFileA(Filename, &FindData);
+    if (FindHandle != INVALID_HANDLE_VALUE)
+    {
+        LastWriteTime = FindData.ftLastWriteTime;
+        FindClose(FindHandle);
+    }
+
+    return(LastWriteTime);
+}
+// MOVE THE WINDOWS STUFF TO PLATFORM LAYER
+
 int Main(int argc, char** argv)
 {
+    // NOTE(casey): Never use MAX_PATH in code that is user-facing, because it
+    // can be dangerous and lead to bad results.
+    char EXEFileName[MAX_PATH];
+    DWORD SizeOfFilename = GetModuleFileNameA(0, EXEFileName, sizeof(EXEFileName));
+    char* OnePastLastSlash = EXEFileName;
+    for (char* Scan = EXEFileName;
+        *Scan;
+        ++Scan)
+    {
+        if (*Scan == '\\')
+        {
+            OnePastLastSlash = Scan + 1;
+        }
+    }
+
+    char SourceGameCodeDLLFilename[] = "handmade.dll";
+    char SourceGameCodeDLLFullPath[MAX_PATH];
+    CatStrings(OnePastLastSlash - EXEFileName, EXEFileName,
+        sizeof(SourceGameCodeDLLFilename) - 1, SourceGameCodeDLLFilename,
+        sizeof(SourceGameCodeDLLFullPath), SourceGameCodeDLLFullPath);
+
+    char TempGameCodeDLLFilename[] = "handmade_temp.dll";
+    char TempGameCodeDLLFullPath[MAX_PATH];
+    CatStrings(OnePastLastSlash - EXEFileName, EXEFileName,
+        sizeof(TempGameCodeDLLFilename) - 1, TempGameCodeDLLFilename,
+        sizeof(TempGameCodeDLLFullPath), TempGameCodeDLLFullPath);
+
     GameDLLProAdresses GameFunctions;
     HINSTANCE hinstLib;
     LinkGameLibrary(hinstLib, GameFunctions);
@@ -85,6 +153,16 @@ int Main(int argc, char** argv)
     
     while (g_GameEngineRunning)
     {
+        FILETIME NewDLLWriteTime = Win32GetLastWriteTime(SourceGameCodeDLLFullPath);
+        if (CompareFileTime(&NewDLLWriteTime, &Game.DLLLastWriteTime) != 0)
+        {
+            Win32UnloadGameCode(&Game);
+            Game = Win32LoadGameCode(SourceGameCodeDLLFullPath,
+                TempGameCodeDLLFullPath);
+            LoadCounter = 0;
+        }
+
+
         GameFunctions.UpdateGame(0.0166f);
     }
 
