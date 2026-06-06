c31bfb5e9d45d1820ac80a503de38d83b53fa275
Andrea Passini <andreapa98@gmail.com>
2026-06-06 19:56:34 +0200

    Fix Align Forward compile error

--- DROPEngine/src/Memory/memoryAllocatorUtils.cpp
+++ DROPEngine/src/Memory/memoryAllocatorUtils.cpp
@@ -1,5 +1,3 @@
-#pragma once
-
 #include <cassert>
 #include "Types/Types.h"
 
@@ -12,7 +10,9 @@
 // Otherwise, if you just need to store "a pointer to something," use a void*.
 bool IsPowerOfTwo(
 	uintptr_t x
-);
+) {
+	return (x & (x - 1)) == 0;
+}
 
 // To align a memory address to the specified alignment is simple modulo arithmetic. 
 // You are looking to find how many bytes forward you need to go 
@@ -20,4 +20,20 @@ bool IsPowerOfTwo(
 uintptr_t AlignForward(
 	uintptr_t ptr
 	, size_t align
-);
+) {
+	uintptr_t p, a, modulo;
+
+	assert(IsPowerOfTwo(align));
+
+	p = ptr;
+	a = (uintptr_t)align;
+	// Same as (p % a) but faster as 'a' is a power of two
+	modulo = p & (a - 1);
+
+	if (modulo != 0) {
+		// If 'p' address is not aligned, push the address to the
+		// next value which is aligned
+		p += a - modulo;
+	}
+	return p;
+}
